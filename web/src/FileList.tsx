2964904788515ccc1899b15b4229cbb02e366144
Borys Ihnatiev <borys.ihnatyev@gmail.com>
2025-06-07 17:12:09 +0300

    No files fallback

--- web/src/FileList.tsx
+++ web/src/FileList.tsx
@@ -12,27 +12,31 @@ type Props = {
 export default function FileList({ files, onChange }: Props) {
   return (
     <>
-      <Heading>Files</Heading>
-      <ul className="flex flex-col gap-y-4">
-        {files?.map((info) => (
-          <li key={info.href} className="flex place-items-center gap-2">
-            <Link href={info.href} className="flex-1">
-              {info.relativePath}
-            </Link>
-            <Button
-              className="flex-0.5"
-              onClick={async () => {
-                await api.content.deleteFile.mutate({
-                  fileName: info.basename,
-                });
-                onChange?.();
-              }}
-            >
-              Delete
-            </Button>
-          </li>
-        ))}
-      </ul>
+      <Heading>
+        {files?.length ? "Content files" : "Content has no files"}
+      </Heading>
+      {files && (
+        <ul className="flex flex-col gap-y-4">
+          {files.map((info) => (
+            <li key={info.href} className="flex place-items-center gap-2">
+              <Link href={info.href} className="flex-1">
+                {info.relativePath}
+              </Link>
+              <Button
+                className="flex-0.5"
+                onClick={async () => {
+                  await api.content.deleteFile.mutate({
+                    fileName: info.basename,
+                  });
+                  onChange?.();
+                }}
+              >
+                Delete
+              </Button>
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
